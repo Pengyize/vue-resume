cc8cc2f074a424a11cbdfbe54a92dbf68202b9a8
pengyize <649786395@qq.com>
2018-05-26 15:19:14 +0800

    first

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        editingName: false,
+        resume: {
+            name: '姓名',
+            gender: '女',
+            birthday: '1996年5月25',
+            jobTitle: '前端工程师',
+            phone: '123456789',
+            email: '123@qq.com'
+        }
+    },
+    methods: {
+        onEdit(key, value) {
+            this.resume[key] = value;
+            console.log('app', app.resume);
+        },
+        onClickSave() {
+            let currentUser = AV.User.current();
+            if (!currentUser) {
+                this.showLogin();
+            }
+            else {
+                this.saveResume()
+            }
+        },
+        showLogin() {
+
+        },
+        saveResume() {
+
+        }
+
+    }
+});
---
+++ src/js/av.js
@@ -0,0 +1,7 @@
+let APP_ID = 'bwmpsHrd8ok4vRRbty2CwD0H-gzGzoHsz';
+let APP_KEY = 'grGDwEdGsP8WsfeaQHrkgghl';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
