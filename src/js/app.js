0e4888796dea4ec5b305d87131aa321910c9fa50
pengyize <649786395@qq.com>
2018-05-26 19:27:03 +0800

    添加登陆、注册、登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,10 @@ let app = new Vue({
         editingName: false,
         loginVisible: false,
         signUpVisible: false,
+        login: {
+            email: '',
+            password: ''
+        },
         resume: {
             name: '姓名',
             gender: '女',
@@ -11,6 +15,10 @@ let app = new Vue({
             jobTitle: '前端工程师',
             phone: '123456789',
             email: '123@qq.com'
+        },
+        signUp: {
+            email: '',
+            password: ''
         }
     },
     methods: {
@@ -18,16 +26,52 @@ let app = new Vue({
             this.resume[key] = value;
             console.log('app', app.resume);
         },
+        onLogin(){
+            AV.User.logIn(this.login.email, this.login.password).then(function (user) {
+                console.log(user);
+            }, function (error) {
+                if(error.code === 211){
+                    alert('邮箱不存在')
+                }else if(error.code === 210){
+                    alert('邮箱或密码错误')
+                }
+            });
+        },
+        onSignUp(){
+            const user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.signUp.email);
+            // 设置密码
+            user.setPassword(this.signUp.password);
+            // 设置邮箱
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (loggedInUser) {
+                console.log(loggedInUser);
+            }, function (error) {
+            });
+        },
         onClickSave() {
             let currentUser = AV.User.current();
             if (!currentUser) {
                 this.loginVisible = true
-
             }
             else {
+                this.saveResume()
             }
         },
-
+        saveResume(){
+            let {id} = AV.User.current()
+            let user = AV.Object.createWithoutData('User', id);
+            // 修改属性
+            user.set('resume', this.resume);
+            // 保存到云端
+            user.save();
+        },
+        onLogOut(){
+            AV.User.logOut();
+            alert('注销成功')
+            window.location.reload()
+        }
 
     }
 });
